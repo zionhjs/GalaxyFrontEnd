8629b3fccabbf34ba6f7e37e62c73a71d60dae04
王永星 <447166939@qq.com>
2021-02-20 10:03:25 +0800

    bak

--- src/pages/home.js
+++ src/pages/home.js
@@ -32,7 +32,7 @@ import { useMediaQuery } from 'react-responsive'
 import MobileHome from '../mobile/Home'
 const HomePage=(props)=> {
   const {profiles,illustration,stillImages,aniImages,dispatch}=props;
-  console.log('updated at 2021/2/19')
+  console.log('updated at 2021/2/20')
   //const isMobile = useMediaQuery({ maxWidth: 767 })
   const isMobile=window.screen.width<768
   const toPage=useCallback(item=>{
