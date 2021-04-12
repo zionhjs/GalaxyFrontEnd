0aa0a384240ffd4270fa676c5b2a7075385df364
王永星 <447166939@qq.com>
2021-04-12 08:55:58 +0800

    bak

--- src/pages/Hidden/index.js
+++ src/pages/Hidden/index.js
@@ -1,15 +1,17 @@
 import React, { useCallback, useEffect } from 'react';
 import classnames from 'classnames'
 import {connect} from 'dva'
-import ImageSwiper from '@/components/ImageSwiper'
-import NavBar from '@/components/NavBar'
+import NavBar from '@/components/ImageHiddenNavBar'
 import styles from './index.css'
-import { Element, OverPack } from 'rc-scroll-anim/es';
+import { OverPack } from 'rc-scroll-anim/es';
 import TweenOne from 'rc-tween-one';
 import LazyLoad from 'react-lazyload';
+import router from 'umi/router'
 import HiddenMobile from '../../mobile/Hidden'
 const Hidden=props=>{
-  const {currentCate,dispatch,col1,col2,col3,col4}=props
+  const {currentCate,dispatch,col1,col2,col3,col4,
+    starNavButtons,starCurrentNav,galaxyNavButtons,
+    galaxyCurrentNav,universeNavButtons,universeCurrentNav}=props
   const isMobile=window.screen.width<768
   useEffect(()=>{
     dispatch({type:'imageHidden/getImage'})
@@ -34,15 +36,35 @@ const Hidden=props=>{
   const universeLoadMore=useCallback(()=>{
 
   },[])
+  //todo
+  const starHandleClick=useCallback((item,index)=>{
+    dispatch({type:'imageHidden/setStarCurrentNav',payload: index})
+    if(index==4){
+      router.push('/hidden/animation-quotation')
+    }
+  },[])
+  //todo
+  const galaxyHandleClick=useCallback((item,index)=>{
+    dispatch({type:'imageHidden/setGalaxyCurrentNav',payload:index})
+    if(index==4){
+      router.push('/hidden/animation-quotation')
+    }
+  },[])
+  const universeHandleClick=useCallback((item,index)=>{
+    dispatch({type:'imageHidden/setUniverseCurrentNav',payload:index})
+    if(index==4){
+      router.push('/hidden/animation-quotation')
+    }
+  },[])
   return isMobile ? <HiddenMobile /> : (
     <div className={styles.container}>
-      <NavBar/>
       <div className={styles.category}>
         <div onClick={handleClick.bind(null,'star')} className={classnames(styles['category-item'],{[styles['active-category']]:currentCate==='star'})}>Star</div>
         <div onClick={handleClick.bind(null,'galaxy')} className={classnames(styles['category-item'],styles['galaxy-item'],{[[styles['active-category']]]:currentCate==='galaxy'})}>Galaxy</div>
         <div onClick={handleClick.bind(null,'universe')} className={classnames(styles['category-item'],{[styles['active-category']]:currentCate==='universe'})}>Universe</div>
       </div>
       <div id={'star'} className={styles.titleBox}>Star - Good Quality</div>
+      <NavBar navButtons={starNavButtons} currentNav={starCurrentNav} handleClick={starHandleClick}/>
       <div className={styles['star-container']}>
         <div className={styles['image-container']}>
         <div className={styles.col1}>
@@ -121,6 +143,7 @@ const Hidden=props=>{
         <div onClick={starLoadMore} className={styles.loadMore}><img src="/loadMore.png" className={styles.loadMoreImg} /><img src='/loadMoreText.png' className={styles.loadMoreText} /></div>
       </div>
       <div id={'galaxy'} className={styles.titleBox}>Galaxy - Excellent Quality</div>
+      <NavBar navButtons={galaxyNavButtons} currentNav={galaxyCurrentNav} handleClick={galaxyHandleClick}/>
       <div className={styles['galaxy-container']}>
         <div className={styles['image-container']}>
           <div className={styles.col1}>
@@ -199,6 +222,7 @@ const Hidden=props=>{
         <div onClick={galaxyLoadMore} className={styles.loadMore}><img src="/loadMore.png" className={styles.loadMoreImg} /><img src="/loadMoreText.png" className={styles.loadMoreText} /></div>
       </div>
       <div id={'universe'} className={styles.titleBox}>Universe - Superior Quality</div>
+      <NavBar navButtons={universeNavButtons} currentNav={universeCurrentNav} handleClick={universeHandleClick}/>
       <div className={styles['universe-container']}>
         <div className={styles['image-container']}>
           <div className={styles.col1}>
@@ -279,4 +303,4 @@ const Hidden=props=>{
     </div>
   )
 }
-export default connect(({imageHidden:{currentCate,col1,col2,col3,col4}})=>({currentCate,col1,col2,col3,col4}))(Hidden)
+export default connect(({imageHidden:{currentCate,col1,col2,col3,col4,starCurrentNav,starNavButtons,galaxyCurrentNav,galaxyNavButtons,universeCurrentNav,universeNavButtons}})=>({currentCate,col1,col2,col3,col4,starCurrentNav,starNavButtons,galaxyCurrentNav,galaxyNavButtons,universeCurrentNav,universeNavButtons}))(Hidden)
