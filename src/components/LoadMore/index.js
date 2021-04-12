b1fc4a0d0c4e01211868cbf3dfc6305b85088624
王永星 <447166939@qq.com>
2021-04-13 06:33:55 +0800

    bak

--- src/components/LoadMore/index.js
+++ src/components/LoadMore/index.js
@@ -5,13 +5,25 @@
  * @LastEditTime: 2020-11-12 00:53:51
  * @FilePath: \test\src\components\LoadMore\index.js
  */
-import React from 'react'
+import React,{useState,useCallback} from 'react'
 import TweenOne from 'rc-tween-one';
 import { OverPack } from 'rc-scroll-anim';
+import {connect} from 'dva'
 import styles from './index.css'
 
-export default function(props){
-    const {loadMore}=props;
+const LoadMore=(props)=>{
+    const {loadMore,dispatch}=props;
+    const [phone,setPhone]=useState('')
+  const [email,setEmail]=useState('')
+  const phoneChange=useCallback((e)=>{
+    setPhone(e.target.value)
+  },[])
+  const emailChange=useCallback(e=>{
+    setEmail(e.target.value)
+  },[])
+  const handleSubmit=useCallback(()=>{
+    dispatch({type:'chat/subscribe',payload:{userEmail:email,userNumber:phone}})
+  },[email,phone])
     return (
         <React.Fragment>
             <div onClick={loadMore} className={styles.loadMore}><img src="loadMore.png" className={styles.loadMoreImg} /><img src="loadMoreText.png" className={styles.loadMoreText} /></div>
@@ -24,11 +36,12 @@ export default function(props){
                 </TweenOne>
 
                 <TweenOne key="form" animation={{ y: '+=50',opacity: 0,type: 'from', ease: "easeInCirc"}} className={styles.boxRight}>
-                    <input className={styles.nameInput} placeholder="Phone" />
-                    <input className={styles.emailInput} placeholder="Email" />
-                    <div className={styles.signBtn}>Sign Me up</div>
+                    <input value={phone} onChange={phoneChange} className={styles.nameInput} placeholder="Phone" />
+                    <input value={email} onChange={emailChange} className={styles.emailInput} placeholder="Email" />
+                    <div onClick={handleSubmit} className={styles.signBtn}>Sign Me up</div>
                 </TweenOne>
             </OverPack>
         </React.Fragment>
     )
 }
+export default connect()(LoadMore)
--- src/mobile/components/GetNews/index.js
+++ src/mobile/components/GetNews/index.js
@@ -5,20 +5,32 @@
  * @LastEditTime: 2020-12-13 08:24:45
  * @FilePath: \GalaxyFrontEnd\src\mobile\components\GetNews\index.js
  */
-import React from 'react'
+import React,{useState,useCallback} from 'react'
 import {connect} from 'dva'
 import { OverPack } from 'rc-scroll-anim';
 import TweenOne from 'rc-tween-one';
 import styles from './index.css'
 const GetNews=(props)=>{
+  const {dispatch}=props
+  const [phone,setPhone]=useState('')
+  const [email,setEmail]=useState('')
+  const phoneChange=useCallback((e)=>{
+    setPhone(e.target.value)
+  },[])
+  const emailChange=useCallback(e=>{
+    setEmail(e.target.value)
+  },[])
+  const handleSubmit=useCallback(()=>{
+    dispatch({type:'chat/subscribe',payload:{userEmail:email,userNumber:phone}})
+  },[email,phone])
     return (
         <OverPack playScale={0.3}>
         <TweenOne animation={{ y: '+=50',opacity: 0,type: 'from', ease: "easeInCirc"}} key="getnews" className={styles.container}>
             <div className={styles.title}>Get News</div>
             <div className={styles.text}>GalaxyCGI is an Architectural Visualization Company which focused on provide affordable photorealistic CG imagery &amp; Animation for marketing campaigns and design presentations</div>
-            <input className={styles.nameInput} placeholder="Name" />
-            <input className={styles.emailInput} placeholder="email" />
-            <div className={styles.signBtn}>Sign Me up</div>
+            <input value={phone} onChange={phoneChange} className={styles.nameInput} placeholder="Phone" />
+            <input value={email} onChange={emailChange} className={styles.emailInput} placeholder="Email" />
+            <div onClick={handleSubmit} className={styles.signBtn}>Sign Me up</div>
         </TweenOne>
         </OverPack>
     )
